28f932fae1a31cefacf7d6bf1f631178b04f4c7e
Cole Dahlen <coledahlen@gmail.com>
2022-12-08 14:36:43 -0600

    can push player guesses into server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,33 @@
 $(document).ready(handleReady);
 
 function handleReady() {
+  $('#submitButton').on('click', submitGuesses);
   console.log("jquery is loaded!")
+}
+function submitGuesses(){
+  let player1Input = $('#player1').val();
+  let player2Input = $('#player2').val();
+  let player3Input = $('#player3').val();
+  let player4Input = $('#player4').val();
+  let playerGuesses = {
+    player1Input,
+    player2Input,
+    player3Input,
+    player4Input 
+  }
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: playerGuesses
+}).then((response) =>{
+    
+})
+}
+function printHistory(){
+  $.ajax({
+    method: 'GET',
+    url: '/history'
+}).then((response) =>{
+    
+})
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const randomNumber = require('./randomNumber.js')
 const app = express();
 const PORT = 5000;
-
+let generateAnswer = randomNumber(0, 25);
+let player1Array = [];
+let player2Array = [];
+let player3Array = [];
+let player4Array = [];
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,8 +15,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
-
-
+app.post('/guesses', (req, res) => {
+  let players = req.body
+  player1Array.push(players.player1Input);
+  player2Array.push(players.player2Input);
+  player3Array.push(players.player3Input);
+  player4Array.push(players.player4Input);
+  res.sendStatus(201);
+})
+app.get('/history', (req, res) => {
+  res.send();
+})
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
