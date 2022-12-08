3e19bf193ba21f4ecd1c2424fbfa274bd1ec1918
Cole Dahlen <coledahlen@gmail.com>
2022-12-08 15:32:16 -0600

    Can test if input is number server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,18 @@
 $(document).ready(handleReady);
-
+let round = 0;
 function handleReady() {
   $('#submitButton').on('click', submitGuesses);
+  $('#submitButton').on('click', printHistory1);
+  $('#submitButton').on('click', printHistory2);
+  $('#submitButton').on('click', printHistory3);
+  $('#submitButton').on('click', printHistory4);
+  
+
   console.log("jquery is loaded!")
 }
 function submitGuesses(){
+  round++;
+  $('#rounds').text(round);
   let player1Input = $('#player1').val();
   let player2Input = $('#player2').val();
   let player3Input = $('#player3').val();
@@ -15,6 +23,10 @@ function submitGuesses(){
     player3Input,
     player4Input 
   }
+  $('#player1').val('');
+  $('#player2').val('');
+  $('#player3').val('');
+  $('#player4').val('');
   $.ajax({
     method: 'POST',
     url: '/guesses',
@@ -23,11 +35,42 @@ function submitGuesses(){
     
 })
 }
-function printHistory(){
+function printHistory1(){
   $.ajax({
     method: 'GET',
-    url: '/history'
+    url: '/history1'
 }).then((response) =>{
-    
+    console.log(response)
+    $('#player1History').text(response)
 })
 }
+function printHistory2(){
+  $.ajax({
+    method: 'GET',
+    url: '/history2'
+}).then((response) =>{
+    console.log(response)
+    $('#player2History').text(response)
+})
+}
+function printHistory3(){
+  $.ajax({
+    method: 'GET',
+    url: '/history3'
+}).then((response) =>{
+    console.log(response)
+    $('#player3History').text(response)
+
+})
+}
+function printHistory4(){
+  $.ajax({
+    method: 'GET',
+    url: '/history4'
+}).then((response) =>{
+    console.log(response)
+    $('#player4History').text(response)
+})
+}
+
+
--- server/server.js
+++ server/server.js
@@ -3,11 +3,60 @@ const bodyParser = require('body-parser')
 const randomNumber = require('./randomNumber.js')
 const app = express();
 const PORT = 5000;
-let generateAnswer = randomNumber(0, 25);
+let generateAnswer = randomNumber(1, 25);
+console.log(generateAnswer)
 let player1Array = [];
 let player2Array = [];
 let player3Array = [];
 let player4Array = [];
+let player1Win = '';
+let player2Win = '';
+let player3Win = '';
+let player4Win = '';
+let playerWinObject = {
+  player1Win,
+  player2Win,
+  player3Win,
+  player4Win
+}
+
+
+function clearPlayers(){
+  player1Array = [];
+  player2Array = [];
+  player3Array = [];
+  player4Array = [];
+}
+function checkPlayerNumber(){
+  for(player of player1Array){
+    if(Number(player) === generateAnswer){
+      console.log('got it1')
+      clearPlayers();
+      playerWinObject.player1Win = 'Player 1 Wins!'
+    }
+  }
+  for(player of player2Array){
+    if(Number(player) === generateAnswer){
+      console.log('got it2')
+      clearPlayers();
+      playerWinObject.player2Win = 'Player 2 Wins!'
+    }
+  }
+  for(player of player3Array){
+    if(Number(player) === generateAnswer){
+      console.log('got it3')
+      clearPlayers();
+      playerWinObject.player3Win = 'Player 3 Wins!'
+    }
+  }
+  for(player of player4Array){
+    if(Number(player) === generateAnswer){
+      console.log('got it4')
+      clearPlayers();
+      playerWinObject.player4Win = 'Player 4 Wins!'
+    }
+  }
+}
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -21,11 +70,24 @@ app.post('/guesses', (req, res) => {
   player2Array.push(players.player2Input);
   player3Array.push(players.player3Input);
   player4Array.push(players.player4Input);
+  checkPlayerNumber();
   res.sendStatus(201);
 })
-app.get('/history', (req, res) => {
-  res.send();
+app.get('/history1', (req, res) => {
+  res.send(player1Array);
+})
+app.get('/history2', (req, res) => {
+  res.send(player2Array);
+})
+app.get('/history3', (req, res) => {
+  res.send(player3Array);
+})
+app.get('/history4', (req, res) => {
+  res.send(player4Array);
 })
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
+})
+app.get('/win1', (req, res) => {
+  res.send()
 })
